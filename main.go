0856bd6314bf038ae7db22a63b3872dbb01981a5
SXFreell <xfreell@163.com>
2024-01-17 03:24:07 +0800

    feat: 修改前后端为相同端口

--- main.go
+++ main.go
@@ -3,7 +3,6 @@ package main
 import (
 	_ "sharebox/dao"
 	"sharebox/service"
-	"sharebox/utils"
 
 	"github.com/kataras/iris/v12"
 	"github.com/robfig/cron/v3"
@@ -11,65 +10,57 @@ import (
 
 func main() {
 	// Web server
-	webServer := iris.New()
-	webServer.Use(iris.Compression)
-	webServer.Use(corsMiddleware())
-	webServer.HandleDir("/", "./static/dist")
-	webServer.OnAnyErrorCode(func(ctx iris.Context) {
+	// webServer := iris.New()
+	// webServer.Use(iris.Compression)
+	// webServer.Use(corsMiddleware())
+	// webServer.HandleDir("/", "./static/dist")
+	// webServer.OnAnyErrorCode(func(ctx iris.Context) {
+	// 	if ctx.GetStatusCode() == iris.StatusNotFound {
+	// 		ctx.ServeFile("./static/dist/index.html")
+	// 	} else {
+	// 		ctx.WriteString(iris.StatusText(ctx.GetStatusCode()))
+	// 	}
+	// })
+
+	// go func() {
+	// 	webServer.Listen(":41251")
+	// }()
+
+	// API
+	app := iris.New()
+	app.Use(iris.Compression)
+	app.Use(corsMiddleware())
+
+	api := app.Party("/api")
+	{
+		authAPI := api.Party("/auth")
+		{
+			authAPI.Get("/", returnIndex)
+			authAPI.Post("/login", service.Login)
+			// authAPI.Post("/register", register)
+			authAPI.Post("/logout", service.Logout)
+		}
+
+		getAPI := api.Party("/get")
+		{
+			getAPI.Get("/", returnIndex)
+			getAPI.Post("/", service.GetSomething)
+		}
+
+		setAPI := api.Party("/set")
+		{
+			setAPI.Get("/", returnIndex)
+			setAPI.Post("/", service.SetSomething)
+		}
+	}
+
+	app.HandleDir("/", "./static/dist")
+	app.OnAnyErrorCode(func(ctx iris.Context) {
 		if ctx.GetStatusCode() == iris.StatusNotFound {
 			ctx.ServeFile("./static/dist/index.html")
 		} else {
 			ctx.WriteString(iris.StatusText(ctx.GetStatusCode()))
 		}
-	})
-
-	go func() {
-		webServer.Listen(":41251")
-	}()
-
-	// booksAPI := app.Party("/books")
-	// {
-	// 	booksAPI.Use(iris.Compression)
-
-	// 	// GET: http://localhost:8080/books
-	// 	booksAPI.Get("/", list)
-	// 	// POST: http://localhost:8080/books
-	// 	booksAPI.Post("/", create)
-	// }
-
-	// API
-	app := iris.New()
-	// app.Use(iris.Compression)
-	app.Use(corsMiddleware())
-	app.Options("{root:path}", func(ctx iris.Context) {
-		ctx.StatusCode(iris.StatusNoContent)
-	})
-
-	authAPI := app.Party("/auth")
-	{
-		authAPI.Post("/login", service.Login)
-		// authAPI.Post("/register", register)
-		authAPI.Post("/logout", service.Logout)
-	}
-
-	getAPI := app.Party("/get")
-	{
-		getAPI.Post("/", service.GetSomething)
-	}
-
-	setAPI := app.Party("/set")
-	{
-		setAPI.Post("/", service.SetSomething)
-	}
-
-	app.Get("/api", func(ctx iris.Context) {
-		ctx.JSON(iris.Map{"message": "Hello Iris!"})
-		utils.Log.Info("Hello Iris!", nil)
-	})
-
-	app.Get("/clear", func(ctx iris.Context) {
-		ctx.JSON(iris.Map{"message": "Hello Iris!"})
-		service.ClearExpiredFile()
 	})
 
 	c := cron.New(cron.WithSeconds())
@@ -94,39 +85,7 @@ func corsMiddleware() iris.Handler {
 	}
 }
 
-// // Book example.
-// type Book struct {
-// 	Title string `json:"title"`
-// }
-
-// func list(ctx iris.Context) {
-// 	books := []Book{
-// 		{"Mastering Concurrency in Go"},
-// 		{"Go Design Patterns"},
-// 		{"Black Hat Go"},
-// 	}
-
-// 	ctx.JSON(books)
-// 	// TIP: negotiate the response between server's prioritizes
-// 	// and client's requirements, instead of ctx.JSON:
-// 	// ctx.Negotiation().JSON().MsgPack().Protobuf()
-// 	// ctx.Negotiate(books)
-// }
-
-// func create(ctx iris.Context) {
-// 	var b Book
-// 	err := ctx.ReadJSON(&b)
-// 	// TIP: use ctx.ReadBody(&b) to bind
-// 	// any type of incoming data instead.
-// 	if err != nil {
-// 		ctx.StopWithProblem(iris.StatusBadRequest, iris.NewProblem().
-// 			Title("Book creation failure").DetailErr(err))
-// 		// TIP: use ctx.StopWithError(code, err) when only
-// 		// plain text responses are expected on errors.
-// 		return
-// 	}
-
-// 	println("Received Book: " + b.Title)
-
-// 	ctx.StatusCode(iris.StatusCreated)
-// }
+// 返回index.html
+func returnIndex(ctx iris.Context) {
+	ctx.ServeFile("./static/dist/index.html")
+}
